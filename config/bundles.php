f976347338de18cbb44ef20d1f4ab0b5abae5251
tcheuD <tcheutcheud@gmail.com>
2020-06-14 16:34:45 +0200

    build(composer) added symfony/maker-bundle --dev

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
